699fadfd0fd56d3d9cfd64ca24379ff330f91071
taey16 <taey1600@gmail.com>
2016-02-19 22:13:31 +0900

    commit

--- tool/_init_paths.py
+++ tool/_init_paths.py
@@ -17,7 +17,8 @@ def add_path(path):
 this_dir = osp.dirname(__file__)
 
 # Add caffe to PYTHONPATH
-caffe_path = osp.join('/works/', 'caffe_build_sys_py', 'python')
+caffe_path = osp.join('/works/', 'caffe', 'python')
+#caffe_path = osp.join('/works/', 'caffe_build_sys_py', 'python')
 add_path(caffe_path)
 print(caffe_path); sys.stdout.flush()
 
