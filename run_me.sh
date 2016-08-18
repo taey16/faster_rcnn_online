903b629dc9db0a845926993b39d868b5d5bfe124
taey16 <taey1600@gmail.com>
2016-08-18 14:49:09 +0900

    remove 11

--- run_me.sh
+++ run_me.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-CUDA_VISIBLE_DEVICES=1 nohup python tool/train_val_net.py --rand --cfg /works/faster_rcnn_online/cfg/11st_All/faster_rcnn_end2end_train_scale_jitter.yml --output ./output > logs.log &
+CUDA_VISIBLE_DEVICES=1 nohup python tool/train_val_net.py --rand --cfg /works/faster_rcnn_online/cfg/faster_rcnn_end2end_train_scale_jitter.yml --output ./output > logs.log &
