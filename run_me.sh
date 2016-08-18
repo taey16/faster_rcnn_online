c3b40484923c80603cacaaa86b56fca34ed32058
taey16 <taey1600@gmail.com>
2016-08-19 08:26:58 +0900

    commit

--- run_me.sh
+++ run_me.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+# example commend
 CUDA_VISIBLE_DEVICES=1 nohup python tool/train_val_net.py --rand --cfg /works/faster_rcnn_online/cfg/faster_rcnn_end2end_train_scale_jitter.yml --output ./output > logs.log &
