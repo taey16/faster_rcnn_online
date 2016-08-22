eb71e5270c283e080f9693d7e5237481c7507afc
taey16 <taey1600@gmail.com>
2016-08-22 21:28:43 +0900

    add

--- run_me.sh
+++ run_me.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-# example commend
+# example commend for train/val
 CUDA_VISIBLE_DEVICES=1 nohup python tool/train_val_net.py --rand --cfg /works/faster_rcnn_online/cfg/faster_rcnn_end2end_train_scale_jitter.yml --output ./output > logs.log &
+# mAP evaluation
+CUDA_VISIBLE_DEVICES=0 python tool/demo.py --cfg cfg/faster_rcnn_end2end_test.yml
