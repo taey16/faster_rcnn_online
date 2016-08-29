b3d42a394a57258ee27b7278d55809d889831d23
taey16 <taey1600@gmail.com>
2016-08-29 15:37:23 +0900

    modify

--- run_me.sh
+++ run_me.sh
@@ -6,5 +6,7 @@ CUDA_VISIBLE_DEVICES=0 nohup python tool/train_val_net.py --rand --cfg /works/fa
 CUDA_VISIBLE_DEVICES=0 python tool/eval.py --cfg cfg/faster_rcnn_end2end_test.yml --output ./result --predict
 # prediction for mAP evaluation (guided detection)
 CUDA_VISIBLE_DEVICES=0 python tool/eval.py --cfg cfg/faster_rcnn_end2end_test.yml --output ./result --guide true --predict
-# mAP evaluation
-CUDA_VISIBLE_DEVICES=0 python tool/eval.py --cfg cfg/faster_rcnn_end2end_test.yml --output ./result --eval
+# mAP evaluation (no-guide)
+python tool/eval.py --cfg cfg/faster_rcnn_end2end_test.yml --output logs_vgg16_predict --eval --guide false
+# mAP evaluation (guide)
+python tool/eval.py --cfg cfg/faster_rcnn_end2end_test.yml --input vgg16_vocdeteval --output logs_resnet-50_predict_guided --eval --guide true
