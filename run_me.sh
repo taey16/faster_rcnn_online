783fb319f104f3caeb9e4ee0dcfaff314a7dcd94
taey16 <taey1600@gmail.com>
2016-08-29 12:53:39 +0900

    add exp for resnet-50

--- run_me.sh
+++ run_me.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
 # example commend for train/val
-CUDA_VISIBLE_DEVICES=1 nohup python tool/train_val_net.py --rand --cfg /works/faster_rcnn_online/cfg/faster_rcnn_end2end_train_scale_jitter.yml --output ./output > logs.log &
+CUDA_VISIBLE_DEVICES=0 nohup python tool/train_val_net.py --rand --cfg /works/faster_rcnn_online/cfg/faster_rcnn_end2end_train_scale_jitter.yml --output ./output > logs.log &
 # prediction for mAP evaluation (non-guided)
 CUDA_VISIBLE_DEVICES=0 python tool/eval.py --cfg cfg/faster_rcnn_end2end_test.yml --output ./result --predict
 # prediction for mAP evaluation (guided detection)
-CUDA_VISIBLE_DEVICES=0 python tool/eval.py --cfg cfg/faster_rcnn_end2end_test.yml --output ./result --guided true --predict
+CUDA_VISIBLE_DEVICES=0 python tool/eval.py --cfg cfg/faster_rcnn_end2end_test.yml --output ./result --guide true --predict
 # mAP evaluation
-python tool/eval.py --cfg cfg/faster_rcnn_end2end_test.yml --output ./result --eval
-# mAP evaluation (guided detection)
-python tool/eval.py --cfg cfg/faster_rcnn_end2end_test.yml --input vgg16_vocdeteval --output vgg16_vocdeteval_guided --eval
+CUDA_VISIBLE_DEVICES=0 python tool/eval.py --cfg cfg/faster_rcnn_end2end_test.yml --output ./result --eval
